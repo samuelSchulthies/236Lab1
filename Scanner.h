cd7b85478c0b0a82a376ba05afce875b336bab1a
samuelSchulthies <samuelschulthies@gmail.com>
2023-01-24 22:20:31 -0700

    final edits for now

--- Scanner.h
+++ Scanner.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Token.h"
 #include <iostream>
+#include <cctype>
 
 using namespace std;
 
@@ -9,6 +10,7 @@ class Scanner {
 private:
     string input;
 
+
 public:
     Scanner(const string& input) : input(input) { }
 
@@ -16,6 +18,27 @@ public:
         TokenType type = COMMA;
         string value = ",";
         int line = 4;
+
+        int length = input.length();
+        for (int i = 0; i < length; i++){
+            int c = input[i];
+
+            //If the first character is a comma, create a COMMA token and erase the first character
+            if (input[0] == ','){
+                Token t = Token(COMMA, ",", 2);
+                input.erase(0);
+            }
+
+            //If the first characters are white space, remove them until gone.
+            //isalpha will break the loop once it stops encountering white space
+            if (isspace(c)){
+                input.erase(i);
+            }
+            if (isalpha(input[i])){
+                break;
+            }
+        }
+
         return Token(type, value, line);
     }
 };
--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include "Token.h"
 #include "Scanner.h"
 
+using namespace std;
+
 int main() {
 //    Token t = Token(COMMA, ",", 2);
 //    cout << t.typeName(COMMA) << endl;
